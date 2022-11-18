7b6850ee33bd082480e9099af909ef3d3c272beb
Mehran Sadeghi <mehran.sadeghi@karnameh.ir>
2022-11-18 16:28:14 +0330

    fix: initial commit. feat: implement sass library and compile it in styles folder.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles/main.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="w-100 row f-lg-column flex-wrap">
+        <div style={{height: '50px', border: '1px solid black'}} className="col-12 col-md-6 col-xl-3 p-2 p-lg-3 pl-xl-5 bg-primary-90">
+          <div className="w-100 h-100 bg-success d-flex f-row justify-center align-items-center"></div>
+        </div>
+        <div style={{height: '50px', border: '1px solid black'}} className="col-12 col-md-6 col-xl-3 p-2 p-lg-3 bg-primary-90">
+          <div className="w-100 h-100 bg-success d-flex f-row justify-center align-items-center"></div>
+        </div>
+        <div style={{height: '50px', border: '1px solid black'}} className="col-12 col-md-6 col-xl-3 p-2 p-lg-3 bg-primary-90">
+          <div className="w-100 h-100 bg-success d-flex f-row justify-center align-items-center"></div>
+        </div>
+        <div style={{height: '50px', border: '1px solid black'}} className="col-12 col-md-6 col-xl-3 p-2 p-lg-3 bg-primary-90">
+          <div className="w-100 h-100 bg-success d-flex f-row justify-center align-items-center"></div>
+        </div>
+        <div style={{height: '50px', border: '1px solid black'}} className="col-12 col-md-6 col-xl-3 p-2 p-lg-3 bg-primary-90">
+          <div className="w-100 h-100 bg-success d-flex f-row justify-center align-items-center"></div>
+        </div>
+        <div style={{height: '50px', border: '1px solid black'}} className="col-12 col-md-6 col-xl-3 p-2 p-lg-3 bg-primary-90">
+          <div className="w-100 h-100 bg-success d-flex f-row justify-center align-items-center"></div>
+        </div>
+        <div style={{height: '50px', border: '1px solid black'}} className="col-12 col-md-6 col-xl-3 p-2 p-lg-3 bg-primary-90">
+          <div className="w-100 h-100 bg-success d-flex f-row justify-center align-items-center"></div>
+        </div>
+        <div style={{height: '50px', border: '1px solid black'}} className="col-12 col-md-6 col-xl-3 p-2 p-lg-3 bg-primary-90">
+          <div className="w-100 h-100 bg-success d-flex f-row justify-center align-items-center"></div>
+        </div>
+      </div>
     </div>
   );
 }
